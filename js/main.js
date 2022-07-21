8574c9f64781ee8c9b1223e4dc4b955b4941ae9c
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-07-21 20:34:37 +0500

    Added modal to plan button and hovers to all links and put hero bgi retina

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 var elOpenModalBtn = document.querySelector(".js-modal-open");
+var elOpenModalTooBtn = document.querySelector(".js-modal-open-too");
 var elOverlay = document.querySelector(".overlay");
 var elOverlayModal = document.querySelector(".overlay-modal");
 var elCloseModalBtn = document.querySelector(".js-modal-close");
 
 elOpenModalBtn.addEventListener("click" , function(){
+   elOverlay.classList.add("modal-open");
+})
+
+elOpenModalTooBtn.addEventListener("click" , function(){
    elOverlay.classList.add("modal-open");
 })
 
