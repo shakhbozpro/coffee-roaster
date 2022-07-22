3b6b5825d4492e19f7e6fec599e71412b741fe00
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-07-22 19:43:12 +0500

    Changed before images' sizes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var elOpenModalBtn = document.querySelector(".js-modal-open");
-var elOpenModalTooBtn = document.querySelector(".js-modal-open-too");
+var elModalOpenBtn = document.querySelector(".js-open-modal");
 var elOverlay = document.querySelector(".overlay");
 var elOverlayModal = document.querySelector(".overlay-modal");
 var elCloseModalBtn = document.querySelector(".js-modal-close");
@@ -8,7 +8,11 @@ elOpenModalBtn.addEventListener("click" , function(){
    elOverlay.classList.add("modal-open");
 })
 
-elOpenModalTooBtn.addEventListener("click" , function(){
+elCloseModalBtn.addEventListener("click" , function(){
+   elOverlay.classList.remove("modal-open");
+})
+
+elModalOpenBtn.addEventListener("click" , function(){
    elOverlay.classList.add("modal-open");
 })
 
